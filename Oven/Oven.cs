0804d5d18e60d2e39e51258520d60f81f715d84a
Bingtao Li <BLi28@slb.com>
2019-06-29 21:13:29 +0800

    Write Oven

---
+++ Oven/Oven.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oven
+{
+    public class Oven
+    {
+        private int temprature = 37;
+        private int threshold;
+        public Oven(int threshold)
+        {
+            this.threshold = threshold;
+            OnTempratureToThreshold += BlowUp;
+        }
+        
+        public void Run()
+        {
+            while (temprature < threshold)
+            {
+                System.Threading.Thread.Sleep(1000);
+                temprature += 1;
+            }
+            OnTempratureToThreshold();
+        }
+
+        private event ThresholdHandler OnTempratureToThreshold;
+        private delegate void ThresholdHandler();
+
+
+        private void BlowUp()
+        {
+            Console.WriteLine("Boom sha ka la ka");
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Account;
+using Oven;
 
 namespace Program
 {
@@ -7,27 +8,30 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            var accounts = new Accounts();
-            var accountsItem1 = new AccountItem("买菜",Category.Spending,10);
-            var accountsItem2 = new AccountItem("买鸡胸",Category.Spending,20, new DateTime(2019,6,1));
-            var accountsItem3 = new AccountItem("美国买菜",Category.Spending,10, Currency.USD);
-            var accountsItem4 = new AccountItem("美国买鸡胸",Category.Spending,20, Currency.USD, new DateTime(2019,6,2));
-            var accountsItem5 = new AccountItem("欧洲买菜",Category.Spending, "我也不知道为啥去欧洲买菜", 10, Currency.EUR);
-            var accountsItem6 = new AccountItem("欧洲买鸡胸",Category.Spending, "我更不知道为啥去欧洲买菜", 20, Currency.EUR, new DateTime(2019,6,2));
-            var accountsItem7 = new AccountItem("工资", Category.Income, "发工资啦，好开心", "但是好像还不起花呗", 300, Currency.RMB, new DateTime(2019,6,18));
-            accounts.Add(accountsItem1);
-            accounts.Add(accountsItem2);
-            accounts.Add(accountsItem3);
-            accounts.Add(accountsItem4);
-            accounts.Add(accountsItem5);
-            accounts.Add(accountsItem6);
-            accounts.Add(accountsItem7);
-            accounts.DisplayTotalExpenditure(new DateTime(2019,6,30));
-            accounts.DisplayTotalIncome(new DateTime(2019,6,30));
-            accounts.DisplayTotalRevenue(new DateTime(2019,6,30));
-            foreach(var info in accounts.Display(new DateTime(2019,6,30))){
-                Console.WriteLine(info.ToString());
-            }
+            //var accounts = new Accounts();
+            //var accountsItem1 = new AccountItem("买菜",Category.Spending,10);
+            //var accountsItem2 = new AccountItem("买鸡胸",Category.Spending,20, new DateTime(2019,6,1));
+            //var accountsItem3 = new AccountItem("美国买菜",Category.Spending,10, Currency.USD);
+            //var accountsItem4 = new AccountItem("美国买鸡胸",Category.Spending,20, Currency.USD, new DateTime(2019,6,2));
+            //var accountsItem5 = new AccountItem("欧洲买菜",Category.Spending, "我也不知道为啥去欧洲买菜", 10, Currency.EUR);
+            //var accountsItem6 = new AccountItem("欧洲买鸡胸",Category.Spending, "我更不知道为啥去欧洲买菜", 20, Currency.EUR, new DateTime(2019,6,2));
+            //var accountsItem7 = new AccountItem("工资", Category.Income, "发工资啦，好开心", "但是好像还不起花呗", 300, Currency.RMB, new DateTime(2019,6,18));
+            //accounts.Add(accountsItem1);
+            //accounts.Add(accountsItem2);
+            //accounts.Add(accountsItem3);
+            //accounts.Add(accountsItem4);
+            //accounts.Add(accountsItem5);
+            //accounts.Add(accountsItem6);
+            //accounts.Add(accountsItem7);
+            //accounts.DisplayTotalExpenditure(new DateTime(2019,6,30));
+            //accounts.DisplayTotalIncome(new DateTime(2019,6,30));
+            //accounts.DisplayTotalRevenue(new DateTime(2019,6,30));
+            //foreach(var info in accounts.Display(new DateTime(2019,6,30))){
+            //    Console.WriteLine(info.ToString());
+            //}
+            //Console.ReadKey();
+            Oven.Oven oven = new Oven.Oven(40);
+            oven.Run();
             Console.ReadKey();
         }
     }
